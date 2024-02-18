a9f05006e63c65a618974caff1dde7d1c7956ef6
AnantShah10 <87380781+AnantShah10@users.noreply.github.com>
2024-02-18 12:44:39 +0530

    hosting

--- aagam_web/aagam_server/database.js
+++ aagam_web/aagam_server/database.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
     user: "root",
     password: "wFgKlq9SeScjWSyb0etIKaf8kAfgyXzx",
-    host: "dpg-cn8q5pgl5elc738v0q8g-a.oregon-postgres.render.com",
+    host: "dpg-cn8q5pgl5elc738v0q8g-a",
     port: 5432,
     database: "aagam_h9mv",
 })
